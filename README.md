a62671e7b522494f5a0fa59d94b1fe372afb77ac
Subject-31415 <168562337+Subject-314159@users.noreply.github.com>
2024-08-11 18:45:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Map tag GPS
+
+Do you also get frustrated that you have to travel long distances and continuously check the map where you are and where you need to go?
+
+This mod is the solution for you! Simply place a map tag with name `'gps-position'` and an arrow will guide you straight to that point!
---
+++ settings.lua
@@ -0,0 +1,25 @@
+data:extend({{
+    type = "int-setting",
+    name = "gps_arrow-size",
+    setting_type = "runtime-global",
+    default_value = 5,
+    minimum_value = 1,
+    maximum_value = 15,
+    order = "a1"
+}, {
+    type = "int-setting",
+    name = "gps_arrow-offset",
+    setting_type = "runtime-global",
+    default_value = 20,
+    minimum_value = 1,
+    maximum_value = 50,
+    order = "a1"
+}, {
+    type = "int-setting",
+    name = "gps_destination-distance",
+    setting_type = "runtime-global",
+    default_value = 5,
+    minimum_value = 2,
+    maximum_value = 15,
+    order = "a1"
+}})
